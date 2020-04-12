1f89c36c663fe5682da7c3f60acce2b990ccc0de
Bhavya-Shah <2679.stkabirnav@gmail.com>
2020-04-12 12:29:30 +0530

    removed semicolon, added spinner, destroy usersub

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faTimes, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-register',
@@ -11,52 +12,64 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.sass']
 })
 export class RegisterComponent implements OnInit {
-  faTimes = faTimes;
-  faExclamationCircle = faExclamationCircle;
-  playVideo: HTMLMediaElement;
-  userSub: Subscription;
+  faTimes = faTimes
+  faExclamationCircle = faExclamationCircle
+  playVideo: HTMLMediaElement
+  userSub: Subscription
+  errorMessage: any = []
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private spinner: NgxSpinnerService
   ) { }
 
   ngOnInit(): void {
-    this.authService.autoLogin();
+    this.authService.autoLogin()
     this.userSub = this.authService.user
       .subscribe(
         user => {
-          const isAuth = !!user;
-          console.log(isAuth);
+          const isAuth = !!user
+          // console.log(isAuth)
           if (isAuth) {
-            this.router.navigate(['/']);
+            this.router.navigate(['/'])
           }
         }
       );
   }
 
   videoPlayer() {
-    this.playVideo = document.querySelector('video');
+    this.playVideo = document.querySelector('video')
     if (this.playVideo.pause) {
-      this.playVideo.muted = true; // important
-      this.playVideo.play();
+      this.playVideo.muted = true // important
+      this.playVideo.play()
     }
-    this.playVideo.classList.add('animated', 'fadeInRightBig');
+    this.playVideo.classList.add('animated', 'fadeInRightBig')
   }
 
   onSubmit(registerForm: NgForm) {
-    console.log(registerForm.value);
-    const username = registerForm.value.username;
-    const email = registerForm.value.email;
-    const password = registerForm.value.password;
+    // console.log(registerForm.value)
+    const username = registerForm.value.username
+    const email = registerForm.value.email
+    const password = registerForm.value.password
+    this.spinner.show()
     this.authService.register(username, email, password)
       .subscribe(
         res => {
-          this.router.navigate(['auth', 'login']);
+          this.spinner.hide()
+          this.router.navigate(['auth', 'login'])
+          this.errorMessage = null
+          // console.log(res)
         },
-        errorObj => {
-          console.log(errorObj);
+        err => {
+          this.spinner.hide()
+          this.errorMessage = err
+          // console.log(err)
         }
-      );
+      )
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe()
   }
 }
