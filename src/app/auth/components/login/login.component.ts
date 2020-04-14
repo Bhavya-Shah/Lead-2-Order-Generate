220adacbfd9131cb5b5692a95a90eb69d0a2a26a
Bhavya-Shah <2679.stkabirnav@gmail.com>
2020-04-15 00:18:19 +0530

    navigating to the dashboard

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -35,7 +35,7 @@ export class LoginComponent implements OnInit, OnDestroy {
           const isAuth = !!user;
           // console.log(isAuth);
           if (isAuth) {
-            this.router.navigate(['/']);
+            this.router.navigate(['/','dashboard']);
           }
         }
       );
